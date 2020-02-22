10b91b02e6b25761b9eba3c50e341c7d8be87460
iheilash <iryna.heilash@ontravelsolutions.com>
2020-02-22 20:13:42 +0300

    init +

---
+++ src/app/profile/campaigns-page/campaigns-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-campaigns-page',
+  templateUrl: './campaigns-page.component.html',
+  styleUrls: ['./campaigns-page.component.scss']
+})
+export class CampaignsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
